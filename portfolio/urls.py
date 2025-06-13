22d5463d3190eca46ab512699ef5534f0047144f
Ajayi Obanijesu <ajayiobanijesu05@gmail.com>
2025-06-13 03:26:01 +0100

    publish

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('meet/', include('video.urls')),
     path('', include('django.contrib.auth.urls')),
     path('', include('estate_mgt.urls')),
 
