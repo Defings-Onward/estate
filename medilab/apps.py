3789bfb3293956818ee9d885f11a493633eda0ab
Ajayi Obanijesu <ajayiobanijesu05@gmail.com>
2025-06-13 03:18:26 +0100

    publish

--- medilab/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MedilabConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'medilab'
--- video/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Example(models.Model):
-    ex_text = models.TextField()
-# Create your models here.
